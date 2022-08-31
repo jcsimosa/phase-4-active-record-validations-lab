62ce0d50fa7792e27e890bb4e93bceed3b4d522f
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-31 16:54:36 -0500

    commit tree e6562e5f5e7f1658232fb8dad906c867d03c6fed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content,length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validates :category,inclusion: { in: ["Fiction", "Non-Fiction"]}
+
+
+    def title_is_clickbait
+        if title.downcase == "True Facts"
+            errors.add(:title, "You Won't Believe These True Facts") 
+        end
+    end
 end
+
